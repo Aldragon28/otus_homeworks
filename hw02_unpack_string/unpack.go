ea8893fdd97176b201b8c77f25d899ddfab6e9a1
aldragon <dimonk280894@gmail.com>
2021-11-27 23:19:13 +0500

    HW2 is completed (version 1)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,71 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+const escapeChar = '\\'
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+var (
+	ErrInvalidString    = errors.New("invalid string")
+	ErrUnexpectBehavior = errors.New("unexpected behavior")
+)
+
+func isSpecial(char rune) bool {
+	return unicode.IsDigit(char) || char == escapeChar
+}
+
+func repeatRune(char rune, count rune) (string, error) {
+	countInt, err := strconv.Atoi(string(count))
+
+	return strings.Repeat(string(char), countInt), err
+}
+
+func Unpack(input string) (string, error) {
+	var result strings.Builder
+
+	casted := []rune(input)
+
+	length := len(casted)
+	for i := 0; i < length; {
+		if unicode.IsDigit(casted[i]) {
+			return "", ErrInvalidString
+		}
+
+		if casted[i] == escapeChar { // nolint:nestif
+			if (i+1 >= length) || !isSpecial(casted[i+1]) {
+				return "", ErrInvalidString
+			}
+
+			if i+2 < length && unicode.IsDigit(casted[i+2]) {
+				str, err := repeatRune(casted[i+1], casted[i+2])
+				if err != nil {
+					panic(ErrUnexpectBehavior)
+				}
+
+				result.WriteString(str)
+				i += 3
+			} else {
+				result.WriteRune(casted[i+1])
+				i += 2
+			}
+		} else {
+			if i+1 < length && unicode.IsDigit(casted[i+1]) {
+				str, err := repeatRune(casted[i], casted[i+1])
+				if err != nil {
+					panic(ErrUnexpectBehavior)
+				}
+
+				result.WriteString(str)
+				i += 2
+			} else {
+				result.WriteRune(casted[i])
+				i++
+			}
+		}
+	}
+
+	return result.String(), nil
 }
